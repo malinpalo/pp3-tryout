1cdff4dc28bd1b129d099d7ecf744eae79e232bc
malinpalo <malinpalo.mp@gmail.com>
2022-12-28 12:08:01 +0000

    The whole

--- run.py
+++ run.py
@@ -1,3 +1,238 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+from graphics import welcome
+from words import hidden_words
+
+
+    
+def welcome_screen():
+    """
+    Option where player kan choose to begin game, select difficulty
+    or view the rules
+    """
+    print (welcome)
+    print (" Press " + "1" + " to play game")
+    print (" Press " + "2" + " to enter difficulty")
+    print(" Press " + "3" + " to view rules")
+    options = False
+    while not options:
+        settings = input("\n ")
+        if settings == "1":
+            options = True
+            difficulty = "default"
+            return difficulty
+
+        elif settings == "2":
+            options = True
+
+        elif settings == "3":
+            options = True
+            game_rules()
+
+        else:
+            print(" Please select 1, 2 or 3 to make your"
+                  " choice")
+
+def difficulty_level():
+    """
+    This is where the player gets to chose difficulty level
+    """
+    print("\n")
+    print("Select Difficulty level\n")
+    print(" Press " + "E" + " for Easy")
+    print(" Press " + "N" + " for Normal")
+    print(" Press " + "H" + " for Hard")
+
+    level = False
+    while not level:
+        difficulty = input("\n").upper()
+        if difficulty == "E":
+            level = True
+            total_lives = 10 
+            return total_lives
+        elif difficulty == "N":
+            level = True
+            total_lives = 7
+            return total_lives
+        elif difficulty == "H":
+            level = True
+            total_lives = 5
+            return total_lives
+        else:
+            print("\n Please select E, N or H to make your choice")
+        
+        
+def get_word():
+    """
+    Gets a word randomly from words.py for player to guess
+    """
+    random_words = random.choice(hidden_words).upper()
+    print('The word has', len(random_words), 'letters')
+
+def game_play(random_words, total_lives):
+    """
+    This is where the game is beeing played.
+    The secret word is beeing displayed during the users turn as underscores.
+    The underscores will be replaced with a letter when the correct letter is guessed. 
+    
+    """
+    secret_word = "_" * len(random_words)
+    game_over = False
+    guesswork = []
+    tries = total_lives
+    print("\n")
+    print("Lets play Hangman!\n")
+    print(f" Lives: {tries}\n")
+    print(f" The word to guess: " + " ".join(secret_word) + "\n" )
+
+    while not game_over and tries > 0:
+        player_guess = input("Guess a letter:\n".upper())
+
+        try:
+            if len(player_guess) > 1:
+                raise ValueError(
+                    f" You can only guess 1 letter at a time, you guessed"
+                    f" {len(player_guess)} characters"
+                )
+            
+            elif not player_guess.isalpha():
+                raise ValueError(
+                    f" You can only guess letters, you guessed {(player_guess)}"
+                    f" which is not a letter"
+                )
+            
+            elif len(player_guess) == 1 and player_try.isalpha():
+                if player_guess in guesswork:
+                    raise ValueError(
+                        f" You have already guessed {(player_guess)}"
+                    )
+
+                elif player_guess not in random_words:
+                    message = f" {(player_guess)} is not in"\
+                              f" the word. You lose a life."
+
+                    guesswork.append(player_guess)
+                    tries -= 1
+
+                else:
+                    info = f" {player_guess} is in the"\
+                              f" word. Well done!"
+
+                    guesswork.append(player_guess)
+                    secret_word_list = list(secret_word)
+                    indi = [i for i, letter in enumerate(word)
+                               if letter == player_guess]
+                    for index in indi:
+                        secret_word_list[index] = player_guess
+                        secret_word = "".join(secret_word_list)
+                    if "_" not in secret_word:
+                        game_over = True
+
+                        
+        except ValueError as e:
+                print(f"Please try again.\n")
+                continue
+
+        print(hangman_tries(tries))
+
+        if lives > 0:
+            print(info)
+            print(f" Lives: {tries}\n")
+            print(f" The word to guess: " + " ".join(secret_word) + "\n")
+            print(" Letters guessed: " + ", ".join(sorted(guesswork)) + "\n")
+
+    if game_over:
+        print(f"Congratulations. {random_words} was the correct" f" word!")
+        the_player_win()
+
+    else:
+        print(f" The correct word was {random_words}")
+        the_hangman_win()
+
+    restart(total_lives)
+    
+
+def restart(total_lives):
+    """
+    The player gets an option to restart the game....
+    """
+    restart = False
+    while not restart:
+        restart_game = input (f" Would you like to play again?" f" Y/N ").upper()
+
+        try:
+            if restart_game == "Y":
+                restart = True
+                hangman_word = get_word()
+
+                game_play(hm_word, total_lives)
+
+            elif restart_game == "N":
+                restart = True
+                print("\n")
+                main()
+
+            else:
+                raise ValueError(
+                    f" You must type in Y or N. You typed {(restart_game)}"
+                )
+
+        except ValueError as e:
+            print(f"\n Please try again.\n")
+
+def player_win():
+    """
+    Display You Win! graphic
+    """
+    print(
+        """
+        __   __
+        \\ \\ / /__  _   _
+         \\ V / _ \\| | | |
+          | | (_) | |_| |
+          |_|\\___/_\\__,_| _
+        __      _(_)_ __ | |
+        \\ \\ /\\ / / | '_ \\| |
+         \\ V  V /| | | | |_|
+          \\_/\\_/ |_|_| |_(_)
+        """ 
+        )
+
+def hangman_win():
+    """
+    Display Game Over! graphic
+    """
+    print(
+        """
+          ____
+         / ___| __ _ _ __ ___   ___
+        | |  _ / _` | '_ ` _ \\ / _ \\
+        | |_| | (_| | | | | | |  __/
+         \\____|\\__,_|_| |_| |_|\\___|
+         / _ \\__   _____ _ __| |
+        | | | \\ \\ / / _ \\ '__| |
+        | |_| |\\ V /  __/ |  |_|
+         \\___/  \\_/ \\___|_|  (_)
+        """ 
+        )
+hangman_win()
+
+def main():
+    """
+    Runs the game
+    """
+    hangman_title()
+    print(hangman_tries(0))
+    level = welcome_screen()
+    if level == "default":
+        num_lives = 7
+    else:
+        num_lives = select_difficulty()
+
+    hm_word = get_random_word()
+    play_game(hangman_word, num_lives)
+
+
+main()
